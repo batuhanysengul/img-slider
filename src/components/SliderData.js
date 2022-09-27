5e53aef58c3fb0854495f5173f337fdc83037bbb
Batuhan <batuhanyigitsengul@gmail.com>
2022-09-27 19:06:55 +0300

    100%

---
+++ src/components/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+    {
+        image: 'https://media.discordapp.net/attachments/1015548474856591402/1024343764292079666/305608738_185511710590627_5754947743765985717_n.jpg?width=537&height=671',
+        alt: 'murcielago'
+    },
+    {
+        image: 'https://media.discordapp.net/attachments/1015548474856591402/1024343764543746158/305679559_1125916811612153_8187549052409321968_n.jpg?width=537&height=671',
+        alt: 'mk4supra'
+    },
+    {
+        image: 'https://media.discordapp.net/attachments/1015548474856591402/1024343764828966962/289902142_147789661174374_8750206090440004623_n.jpg?width=537&height=671',
+        alt: 'r32gtr'
+    },
+
+    {
+        image: 'https://media.discordapp.net/attachments/1015548474856591402/1024343765130940456/283922454_547963150115332_2473855782664158270_n.jpg?width=537&height=671',
+        alt: 'nsx'
+    },
+    {
+        image: 'https://media.discordapp.net/attachments/1015548474856591402/1024343765466488862/301234288_637893324420160_598841335305550390_n.jpg?width=537&height=671',
+        alt: '911turbo'
+    } 
+];
